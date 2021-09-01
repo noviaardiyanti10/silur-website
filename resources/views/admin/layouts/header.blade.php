f3a9e85ec7702743445b564129431c7dd30cec1b
Alitindrawan24 <alitindrawan71@gmail.com>
2021-09-01 23:26:23 +0800

    logout

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -3,12 +3,22 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
     </div>
     <!--logo start-->
-    <a href="index.php" class="logo"><b style=" font-size: 25px;">SILUR</b></a>
+    <a href="{{ url('/home') }}" class="logo"><b style=" font-size: 25px;">SILUR</b></a>
     <!--logo end-->
     <div class="top-menu">
         <ul class="nav pull-right top-menu">
-            <li><a class="logout" onclick="javascript: return confirm('Ingin Logout ?');"
-                    href="logout.php">Logout</a></li>
+            <li><a href="#" class="logout" onclick="logout()">Logout</a></li>
         </ul>
     </div>
 </header>
+
+<form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+    {{ csrf_field() }}
+</form>
+
+<script>
+    function logout(event){
+        confirm('Ingin Logout ?');
+        document.getElementById('frm-logout').submit();
+    }
+</script>
